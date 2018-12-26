d1c4c4904835da64656d9b143e52dd5f0e3dc387
oneheck <writingatyourfeet@gmail.com>
2018-12-26 05:13:12 -0800

    change

--- css/main.js
+++ css/main.js
@@ -17,3 +17,20 @@ $(".owl-carousel").owlCarousel({
     }
   }
 });
+
+var ssBackToTop = function() {
+  var pxShow = 500,
+    goTopButton = $(".gotopbtn");
+
+  // Show or hide the button
+  if ($(window).scrollTop() >= pxShow) goTopButton.addClass("link-is-visible");
+
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop() >= pxShow) {
+      if (!goTopButton.hasClass("link-is-visible"))
+        goTopButton.addClass("link-is-visible");
+    } else {
+      goTopButton.removeClass("link-is-visible");
+    }
+  });
+};
